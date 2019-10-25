4104f3ce05b6888746f380d36a1b69d280515fff
Nesreen <nesreentrgalal@hotmail.com>
2019-10-25 17:56:14 -0400

    EX6: missing D for background in function draw

--- exercises/exercise6/js/script.js
+++ exercises/exercise6/js/script.js
@@ -23,7 +23,8 @@ let bee;
 //
 // Sets up a canvas
 // Creates objects for the predator and three prey
-functionsetup() {
+////////////////// FIXED (space between function and setup )
+function setup() {
   createCanvas(windowWidth, windowHeight);
   tiger = new Predator(100, , 100, 5, color(200, 200, 0), 40);
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
@@ -36,7 +37,9 @@ functionsetup() {
 // Handles input, movement, eating, and displaying for the system's objects
 function draw() {
   // Clear the background to black
-  backgroun(0);
+  ////////////////// FIXED (missing D in background )
+
+  background(0);
 
   // Handle input for the tiger
 
