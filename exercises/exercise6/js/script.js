16caba8a2bf865feee5e1da1b84c6541ec5e09a5
Nesreen <nesreentrgalal@hotmail.com>
2019-10-25 18:19:05 -0400

    EX6: MISSING y parameter for zebra

--- exercises/exercise6/js/script.js
+++ exercises/exercise6/js/script.js
@@ -26,9 +26,9 @@ let bee;
 ////////////////// FIXED (space between function and setup )
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  tiger = new Predator(100,100, 5, color(200, 200, 0), 40); //fixed extra comma removed 
+  tiger = new Predator(100,100, 5, color(200, 200, 0), 40); //FIXED extra comma removed
   antelope = new Prey(100, 100, 10, color(255, 100, 10), 50);
-  zebra = new Prey(100, 8, color(255, 255, 255), 60);
+  zebra = new Prey(100,100,8, color(255, 255, 255), 60); //FIXED missing y parameter for zebra
   bee = new Prey(100, 100, 20, color(255, 255, 0), 10);
 }
 
